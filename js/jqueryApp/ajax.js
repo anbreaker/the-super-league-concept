1a491f35b658fe026fa91518b1a849b6c5167f46
anbreaker <antunez19@gmail.com>
2022-01-11 21:43:07 +0100

    change team and temperature correct

--- js/jqueryApp/ajax.js
+++ js/jqueryApp/ajax.js
@@ -2,36 +2,41 @@
 (function () {
   const latitude = 38.871571;
   const longitude = -6.949143;
+  const shield = 'acmilan_hero_logo.png';
 
-  $.ajax({
-    type: 'GET',
-    url: `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=9f50a805aa0089a1edd1829a5db029f0`,
-    dataType: 'jsonp',
-  })
-    .done(function (data) {
-      console.log('Correct!');
-      console.log(data);
+  getWeather(latitude, longitude, shield);
 
-      showData(data);
-      showStadiumTemperature();
+  function getWeather(latitude, longitude, shield) {
+    $.ajax({
+      type: 'GET',
+      url: `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=9f50a805aa0089a1edd1829a5db029f0`,
+      dataType: 'jsonp',
     })
-    .fail(function () {
-      console.log('Error!');
-    })
-    .always(function () {
-      console.log('Complete!');
-    });
+      .done(function (data) {
+        // console.log('Correct!');
 
-  function showData(data) {
-    // Imagen
+        showData(data, shield);
+        showStadiumTemperature();
+      })
+      .fail(function () {
+        console.log('Error!');
+      })
+      .always(function () {
+        // console.log('Updated weather information!');
+      });
+  }
+
+  function showData(data, shield) {
+    // Image weather
     const urlWeather = `img/weather/${data.weather[0].icon}.png`;
     $('.img-weather').attr('src', urlWeather);
 
-    // TODO url team importar json leer y pintar
-    // const urlTeam = `img/teams/${team}`;
-    // $('.img-team').attr('src', urlTeam);
+    const urlTeam = `img/teams/${shield}`;
+    $('.img-team').attr('src', urlTeam);
 
-    let temperature = Math.round(data.main.temp);
+    $('.stadium').html(shield + '&#176;');
+
+    const temperature = Math.round(data.main.temp);
     $('.temperature').html(temperature + '&#176;');
 
     $('.div-loading').fadeOut(200, function () {
@@ -42,7 +47,33 @@
   function showStadiumTemperature() {
     $('.teams h3').click(function (event) {
       const team = event.target.innerHTML;
-      console.log(team);
+
+      getJsonTeams(team);
     });
+  }
+
+  function getJsonTeams(team) {
+    $.ajax({
+      type: 'GET',
+      dataType: 'json',
+      url: '../../database/dbStadiumsGPS.json',
+    })
+      .done(function (data) {
+        if (data) {
+          // console.log('Json completed successfully!');
+
+          const teamFound = data.filter(function (item) {
+            return item.name === team;
+          });
+
+          getWeather(teamFound[0].latitude, teamFound[0].longitude, teamFound[0].shield);
+        }
+      })
+      .fail(function (jqXHR, textStatus, errorThrown) {
+        console.log('The application has failed: ' + textStatus);
+      })
+      .always(function () {
+        // console.log('Complete!');
+      });
   }
 })();
--- js/jqueryApp/useJquery.js
+++ js/jqueryApp/useJquery.js
@@ -1,6 +1,29 @@
 // Load DOM first
 $(function () {
-  $('.t-madrid').click(function () {
-    // console.log('ver');
-  });
+  $.ajax({
+    type: 'GET',
+    dataType: 'json',
+    url: '../../database/dbStadiumsGPS.json',
+  })
+    .done(function (data, team) {
+      team = null;
+
+      if (data && team) {
+        // TODO
+        // console.log('Json completed successfully!');
+
+        const found = data.filter(function (item, team) {
+          return item.name === team;
+        });
+
+        console.log(found[0].name);
+      }
+    })
+    .fail(function (jqXHR, textStatus, errorThrown) {
+      console.log('The application has failed: ' + textStatus);
+    })
+    .always(function () {
+      // TODO
+      // console.log('Complete!');
+    });
 });
